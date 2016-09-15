97688b36a9947dfcb68809ef7e45d7226aa95414
ACzero <lzh.scut@hotmail.com>
2016-09-15 11:12:57 +0800

    exercise 3 add test

--- husky/test/husky_test.rb
+++ husky/test/husky_test.rb
@@ -1,11 +1,21 @@
-require 'test_helper'
+require_relative 'test_helper'
 
 class HuskyTest < Minitest::Test
+  include Rack::Test::Methods
+
   def test_that_it_has_a_version_number
     refute_nil ::Husky::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def app
+    Husky::Application.new
+  end
+
+  def test_request
+    get "/"
+
+    assert last_response.ok?
+    body = last_response.body
+    assert body["husky!"]
   end
 end
