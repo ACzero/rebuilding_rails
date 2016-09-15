02771da38b355d5fdacbe0a3e8d9db17ab91d1fb
ACzero <lzh.scut@hotmail.com>
2016-09-15 14:27:52 +0800

    exercise 1

--- best_quotes/app/controllers/quotes_controller.rb
+++ best_quotes/app/controllers/quotes_controller.rb
@@ -1,6 +1,8 @@
 class QuotesController < Husky::Controller
   def a_quote
+    env_strs = env.map { |k,v| %Q{ <pre>"#{k}" => "#{v}"</pre> } }
     "There is nothing either good or bad " +
-      "but thinking makes it so."
+      "but thinking makes it so." +
+      %Q[\n#{env_strs.join("\n")}\n]
   end
 end
