ad4242a7eb4a95bc95dffec3a45b037220198fb4
ACzero <lzh.scut@hotmail.com>
2016-09-15 16:46:01 +0800

    fix bug

--- husky/lib/husky/dependencies.rb
+++ husky/lib/husky/dependencies.rb
@@ -2,10 +2,15 @@ class Object
   def self.const_missing(c)
     return super if @calling_const_missing
 
-    @calling_const_missing = true
-    require Husky.to_underscore(c.to_s)
-    klass = Object.const_get(c)
-    @calling_const_missing = false
+    begin
+      @calling_const_missing = true
+      require Husky.to_underscore(c.to_s)
+      klass = Object.const_get(c)
+    rescue Exception => e
+      raise e
+    ensure
+      @calling_const_missing = false
+    end
 
     klass
   end
