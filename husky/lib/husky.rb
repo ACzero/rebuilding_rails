3fe86fd1a94be59b9387931641350da8ba60739f
ACzero <lzh.scut@hotmail.com>
2016-09-15 13:23:59 +0800

    add router

--- husky/lib/husky.rb
+++ husky/lib/husky.rb
@@ -1,10 +1,23 @@
+require 'pry'
 require "husky/version"
 require "husky/array"
 
 module Husky
   class Application
     def call(env)
-      [200, {'Content-Type' => 'text/html'}, ["husky!"]]
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
+
+      [200, {'Content-Type' => 'text/html'}, [text]]
+    end
+  end
+
+  class Controller
+    attr_accessor :env
+
+    def initialize(env)
+      self.env = env
     end
   end
 end
